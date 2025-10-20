c2c9e53ae33cb8af4defcd413c04852712f2e8a7
goblin-code-squad <alessio.pedrotti@refresh-academy.org>
2025-10-20 23:38:47 +0200

    consegno script finale

--- assignments/wk_14_openbo/02_openbo_script_dwh_finale.sql
+++ assignments/wk_14_openbo/02_openbo_script_dwh_finale.sql
@@ -1,5 +1,5 @@
--- drop schema if exists chinook_integration cascade;  
--- questo lo commento xche' vorrei usarlo ma non ho avuto tepo di guardarci
+-- drop schema if exists chinook_integration cascade  
+-- questo lo commento xche' vorrei usarlo ma non ho avuto tempo
 
 SET search_path TO openbo_dwh;
 
@@ -26,7 +26,8 @@ WHERE
 
 -- ok qui ho creato una tabella che non contiene null ma ha un problema perche' non mi casta integer correttamente. ci penso dopo
 
--- questa e' da rifare xche' devo usare la tabella temporanea
+-- questa ho rifatto perche' ho usato adesso la tabella tt
+
 drop table if exists openbo_dwh.dim_tempo_inizio ; 
 create table openbo_dwh.dim_tempo_inizio as
 select distinct
@@ -83,8 +84,8 @@ select distinct
 	
 from openbo_landing.lt_incarichi_di_collaborazione;
 
-
-SELECT
+-- qui ho fatto una prova per vedere maggio
+/*SELECT
     nome_mese,
     COUNT(ids_giorno) AS numero_di_giorni_unici_presenti
 FROM
@@ -96,7 +97,7 @@ ORDER BY
 
 select * from openbo_dwh.dim_tempo_inizio dti 
 where nome_mese = 'May'
-order by dti.ids_giorno 
+order by dti.ids_giorno */
 
 
 -- ora provo a creare la dimensione fine 
@@ -121,9 +122,9 @@ where
     nullif(lt_incarichi_conferiti.data_fine_incarico, '')::date is not null;
 
 
--- controllo che non ci siano duplicati
+-- controllo che non ci siano duplicati e ora lo commento
 
-select
+/*select
     data_sorgente_fine,
     count(*) as conteggio_duplicati
 from
@@ -146,11 +147,11 @@ group by
 having
     count(*) > 1;
     
--- ok qui mi sembra vada tutto bene! yeeeeh. ho il dubbio che sto usando pero' solo la parte di uno e non la union. proviamo a fare la union di tutte e due e dargli una tabella temporanea. ho passato qualche giorno lavorando soltanto e non ricordo cosa dovevo fare quindi ora faccio solo la union
+-- ok qui mi sembra vada tutto bene! yeeeeh. ho il dubbio che sto usando pero' solo la parte di uno e non la union. proviamo a fare la union di tutte e due e dargli una tabella temporanea. 2025_10_20 ho passato qualche giorno lavorando soltanto e non ricordo cosa dovevo fare quindi ora faccio solo la union*/
     
- select tti.data_sorgente from openbo_landing.tt_tempo_inizio tti 
+/* select tti.data_sorgente from openbo_landing.tt_tempo_inizio tti 
  union
- select data_sorgente_fine  from openbo_landing.tt_tempo_fine
+ select data_sorgente_fine  from openbo_landing.tt_tempo_fine*/
  
  --francamente non ho capito il ragionamento che ho fatto e passo oltre
 
@@ -227,25 +228,35 @@ select distinct
 	
 from openbo_integration.tt_tempo_fine;
 
--- qui semplicemente ho copincollato la creazione inizio e messo tutto con "al" invece che dal
+-- qui semplicemente ho copincollato la creazione inizio e messo tutto dalla tabella tt di fine
 
 
--- ho cercato online un aiuto per scrivere dei nest della funzione replace, ho visto che e' possibile nestare un sacco di cose e quindi ho prima fatto "dottore" e poi "dott" ed ho messo tutto lower perche' prendesse tutto. per ora non ho sistemato ada labriola, voglio finire le tabelle
+-- ho cercato online un aiuto per scrivere dei nest della funzione replace, ho visto che e' possibile nestare un sacco di cose e quindi ho prima fatto "dottore" e poi "dott" ed ho messo tutto lower perche' prendesse tutto. per ora non ho sistemato ada labriola, voglio finire le tabelle, devo anche mettere initcap
 
--- ora faccio union x tabella temporanea responsabili pulendo nel mentre spazi e maiuscole minuscole
+-- ora faccio union x tabella temporanea responsabili pulendo nel mentre spazi e maiuscole minuscole che dovrebbe essere la mia tt_
 
+drop table if exists openbo_integration.tt_responsabili;
+create table openbo_integration.tt_responsabili as
 select 
-	coalesce(lower(trim(responsabile)), 'missing/mancante'),
+	coalesce(
+		nullif(lower(trim(responsabile)), ''),
+		'missing/mancante'
+		) as responsabile_pulito,
 	'incarichi_di_collaborazione' as source_system
 	from openbo_landing.lt_incarichi_di_collaborazione lidc 
 
 union
 
-select coalesce(trim(lower(responsabile_della_struttura_conferente)), 'missing/mancante'),
+select coalesce(
+		nullif(lower(trim(responsabile_della_struttura_conferente)), ''),
+		'missing/mancante'
+		) as responsabile_pulito,
 'incarichi_conferiti' as source_system	
-from openbo_landing.lt_incarichi_conferiti lic 
+from openbo_landing.lt_incarichi_conferiti lic;
 
 
+
+-- qui provo un primo abbozzo di dim_responsabile
 
 set search_path to openbo_dwh;
 
@@ -282,18 +293,3 @@ select distinct
 'ETL' as source_system
 
 from openbo_landing.lt_incarichi_di_collaborazione;
-
-
--- cose che restano da fare
-
-/*
-rifare dim_tempo_inizio usando la tabella union
-fare tt_responsabili
-rifare dim_responsabile con tt_resonsabilia
-sistemare ada labriolaa dentro al replace
-sistemare franco charii dentro al replace
-sistemare A.I.*
-
-fare FACT_INCARICHI
-fare ET_INCARICHI_COLLABORAZIONE e ET_INCARICHI_CONFERITI
-fare CHECK_IMPORTI /
